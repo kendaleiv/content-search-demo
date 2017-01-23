848223dc5c82668836ea048637b93a11f0bf4477
Ken Dale <ken@kendaleiv.com>
2017-01-23 12:41:48 -0500

    Serve with Node.js to add authentication

--- deploy.cmd
+++ deploy.cmd
@@ -51,18 +51,62 @@ ECHO CALLING GET RUBY
 
 call :ExecuteCmd "getruby.cmd"
 
-ECHO WE MADE IT
+goto Deployment
+
+:: Utility Functions
+:: -----------------
+
+:SelectNodeVersion
+
+IF DEFINED KUDU_SELECT_NODE_VERSION_CMD (
+  :: The following are done only on Windows Azure Websites environment
+  call %KUDU_SELECT_NODE_VERSION_CMD% "%DEPLOYMENT_SOURCE%" "%DEPLOYMENT_TARGET%" "%DEPLOYMENT_TEMP%"
+  IF !ERRORLEVEL! NEQ 0 goto error
+
+  IF EXIST "%DEPLOYMENT_TEMP%\__nodeVersion.tmp" (
+    SET /p NODE_EXE=<"%DEPLOYMENT_TEMP%\__nodeVersion.tmp"
+    IF !ERRORLEVEL! NEQ 0 goto error
+  )
+
+  IF EXIST "%DEPLOYMENT_TEMP%\__npmVersion.tmp" (
+    SET /p NPM_JS_PATH=<"%DEPLOYMENT_TEMP%\__npmVersion.tmp"
+    IF !ERRORLEVEL! NEQ 0 goto error
+  )
+
+  IF NOT DEFINED NODE_EXE (
+    SET NODE_EXE=node
+  )
+
+  SET NPM_CMD="!NODE_EXE!" "!NPM_JS_PATH!"
+) ELSE (
+  SET NPM_CMD=npm
+  SET NODE_EXE=node
+)
+
+goto :EOF
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Deployment
 :: ----------
 
-echo Handling Basic Web Site deployment.
+:Deployment
+echo Handling node.js deployment.
 
-:: 1. KuduSync
+:: Select node version
+call :SelectNodeVersion
+
+:: KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/_site" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
+)
+
+:: Install npm packages
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! install --production
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
 )
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
---
+++ server.js
@@ -0,0 +1,111 @@
+// https://github.com/passport/express-4.x-openidconnect-example/blob/master/server.js
+
+if (!process.env.SKIP_DOTENV_CONFIG) {
+  require('dotenv').config();
+}
+
+const fs = require('fs');
+const path = require('path');
+const shell = require('shelljs');
+const express = require('express');
+const passport = require('passport');
+const OidcStrategy = require('passport-openidconnect').Strategy;
+
+const port = process.env.port || 3000;
+const publicUrls = process.env.PUBLIC_URLS
+  ? process.env.PUBLIC_URLS.split(/\s*[,;]\s*/)
+  : [];
+
+// Begin serving a potential previous version of the site immediately
+setTimeout(() => {
+  const options = {};
+
+  if (process.env.SILENT) {
+    options.silent = true;
+  }
+
+  shell.exec('bundle exec jekyll build', options);
+});
+
+passport.use(new OidcStrategy({
+  issuer: process.env.ISSUER,
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  authorizationURL: process.env.AUTHORIZATION_URL,
+  tokenURL: process.env.TOKEN_URL,
+  userInfoURL: process.env.USER_INFO_URL,
+  callbackURL: process.env.CALLBACK_URL,
+  scope: 'openid profile email'
+}, verify));
+
+function verify(token, tokenSecret, profile, cb) {
+  return cb(null, profile);
+}
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+const app = express();
+
+if (!process.env.SILENT) {
+  app.use(require('morgan')('combined'));
+}
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({
+  cookie: {
+    httpOnly: true,
+    secure: process.env.EXPRESS_INSECURE === 'true' ? false : true
+  },
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.EXPRESS_SESSION_SECRET
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', passport.authenticate('openidconnect'));
+
+app.get('/callback', passport.authenticate('openidconnect'), (req, res) => {
+  res.redirect(req.session.returnTo || '/');
+});
+
+// Reference: https://github.com/jaredhanson/connect-ensure-login/blob/cdb5769a3db7b8075b2ce90fab647f75f91b3c9a/lib/ensureLoggedIn.js
+app.use((req, res, next) => {
+  if (req.isAuthenticated() || publicUrls.some(u => u && new RegExp(`^${u}`, 'i').test(req.originalUrl))) {
+    next();
+  } else {
+    if (req.session) {
+      req.session.returnTo = req.originalUrl || req.url;
+    }
+
+    res.redirect('/login');
+  }
+});
+
+app.use(express.static(path.join(__dirname, '_site')));
+
+app.use((req, res) => {
+  const http404FilePath = path.join(__dirname, '_site/404.html');
+
+  fs.access(http404FilePath, fs.constants.F_OK, err => {
+    if (err) {
+      res.sendStatus(404);
+    } else {
+      res.status(404).sendFile(http404FilePath);
+    }
+  });
+});
+
+if (!process.env.EXPRESS_NO_LISTEN) {
+  app.listen(port, () => {
+    console.log(`Express listening on port ${port}`); // eslint-disable-line no-console
+  });
+}
+
+module.exports = app;
